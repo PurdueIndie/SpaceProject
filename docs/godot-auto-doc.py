2f25080ef6df2a332af81d19f17866e0af2e7b83
Joseph Marshall <marsha87@purdue.edu>
2017-09-21 07:06:02 -0400

    Big auto-doc bout to hit
    i got it!!! almost done with formatting

--- docs/godot-auto-doc.py
+++ docs/godot-auto-doc.py
@@ -1,58 +1,72 @@
 #! /usr/bin/env python3
 # godot-auto-doc.py - Automatic documentation creator for GDscript
-# usage: ./godot_auto_doc.py <start_dir> <doc_dest_dir>
+# usage: should be in docs/ directory
 
-import os, re
+import re, os, pprint
 
-function_regex = re.compile(r'((#.*\n)*)func +([^_].*):')
-enum_regex = re.compile(r'enum(.*){(([^,}]*,)*.*)}')
-full_regex = re.compile(r'(((#.*\n)*)func +([^_].*):|enum(.*){(([^,}]*,)*.*)})')
+# paths to relevant directories
+path_to_scripts = '../game/script/'
 
-os.chdir('/Users/jmarshall/GitHub/SpaceProject/')
-
-def document_file( file, f , a ):
-    text = file.read()
-    result = full_regex.findall(text)
-    print("\nFile " + str(f))
-    #print(str(text))
-    print("result:" + str(result))
-    #print("e_result" + str(e_result))
-    api_file = a
-    api_file.write(f + '\n')
-    for i in range(len(f)):
-        api_file.write('=')
-    api_file.write('\n\n')
-    for r in result:
-        if len(r) >= 7:
-            if r[6] == '':
-                comments = r[1].split('#')
-                api_file.write('- ' + r[3].strip() + '\n')
-                for i in range(len(comments)):
-                    api_file.write(comments[i].strip() + '\n')
-                api_file.write('\n')
-            elif r[1] == '':
-                api_file.write('- ' + r[4].strip() + '\n\n')
-                iden = r[5].split(',')
-                for i in range(len(iden)):
-                    api_file.write(iden[i].strip() + '\n')
-                api_file.write('\n')
-    api_file.write('\n\n')
-    print('done')
-
+# Get list of all *.gd files to read in files
 file_regex = re.compile(r'.*\.gd')
 files = []
+for folder_name, sub_folders, file_names in os.walk(path_to_scripts):
+	all_file = filter(file_regex.match, list(file_names))
+	for f in (list(all_file)):
+		files.append(f)
 
-for folder_name, sub_folders, file_names in os.walk('game/script'):
-    all_file = filter(file_regex.match, list(file_names))
-    for f in (list(all_file)):
-        files.append(f)
 
-print(files)
-a = open('docs/api.rst', 'w')
-
+# opens one file at a time and stores in file_data
+full_regex = re.compile(r'(((#.*\n)*)func +([^_].*)\((.*)\):|enum(.*){(([^,}]*,)*.*)})')
+comment_regex = re.compile(r'ret +(.*), *arg +(.*)')
+#function_regex = re.compile(r'((#.*\n)*)func +([^_].*):')
+#enum_regex = re.compile(r'enum(.*){(([^,}]*,)*.*)}')
+file_data = {}
 for f in files:
-    file = open('game/script/' + f)
-    document_file(file, f, a)
-    file.close()
+	file_data[f] = []
+	file = open(path_to_scripts + f)
+	text = file.read()
+	file.close()
+	# searches text by regex to get info to put into file_data
+	result_list = full_regex.findall(text)
+	for index, result_groups in enumerate(result_list):
+		if result_groups[3] == '':
+			#enum
+			print("Enum.")
+		else:
+			#func
+			comment_group = result_groups[1].split('\n')
+			function_data = {"function_name": result_groups[3]}
+			comments = ''
+			for i in range(len(comment_group) - 1):
+				if i == len(comment_group) - 2:
+					match = comment_regex.match(comment_group[i][1:].strip())
+					print(match)
+					if match != None:
+						arg_types = match.group(2).split(',')
+						args_raw = result_groups[4].split(',')
+						args = []
+						for i in range(len(args_raw)):
+							if i < len(arg_types):
+								args.append(arg_types[i].strip() + " " + args_raw[i].strip())
+							else:
+								args.append(args_raw[i].strip())
+						function_data["arguments"] = args
+						function_data["return_type"] = match.group(1).strip()
+						continue
+				# need refactoring
+				comments += comment_group[i][1:].strip() + "  \n"
+			if comments != '':
+				function_data["comments"] = comments
+			file_data[f].append(function_data)
 
-a.close()
+
+pprint.pprint(file_data)
+
+# Opens api file in api_file and writes file_data to it
+api_file = open('API.md', 'w')
+
+# writing
+
+
+api_file.close()
